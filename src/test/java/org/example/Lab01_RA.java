a6ca12c6316a4da125942d49d42778ebad290f3f
Prateek Sharma <sharmaprateek24@gmail.com>
2025-07-12 12:25:26 +0530

    Rest Assured-12-7-25

---
+++ src/test/java/org/example/Lab01_RA.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import io.restassured.RestAssured;
+
+public class Lab01_RA {
+    public static void main(String[] args) {
+        RestAssured.given()
+                    .baseUri("https://restful-booker.herokuapp.com/ping")
+                .when()
+                    .get()
+                .then()
+                    .log().all().statusCode(201);
+    }
+}
